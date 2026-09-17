af2d19800847b314e986e80eea0ae5efe07cde15
omarinclan-gif <omarinclan@ciencias.unam.mx>
2026-09-17 10:45:00 -0600

    IVA

--- Clase3/src/Main.java
+++ Clase3/src/Main.java
@@ -2,6 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        float IVA_MEXICO = 0.16f;
+        float precio = 100.0f;
+        float precioConIVA = precio + (precio * IVA_MEXICO);
+        System.out.println("Precio con IVA: " + precioConIVA);
     }
 }
